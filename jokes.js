ffafc4a9009ff87700de8c77b530a6c9e7345306
Cyril Sebastian <Cyril Sebastian>
2016-09-16 16:59:25 +1200

    Add all files and folders

---
+++ jokes.js
@@ -0,0 +1,11 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "https://api.chucknorris.io/jokes/random", true);
+xhr.send();
+xhr.addEventListener("readystatechange", processRequest, false);
+
+function processRequest(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var response = JSON.parse(xhr.responseText);
+        alert(response.value);
+    }
+}
