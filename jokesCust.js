6f10590b4f4b2e9c1b71f984b7b7e396d6128985
Cyril Sebastian <Cyril Sebastian>
2016-09-24 19:06:01 +1200

    Add all files and folders

---
+++ jokesCust.js
@@ -0,0 +1,8 @@
+function jokesCust(){
+    var fName = ($('#fn').val());
+    var lName = ($('#ln').val());
+   
+     $.getJSON('http://api.icndb.com/jokes/random/?firstName= '+fName+'&lastName=+' +lName+'', function(json_data){
+        alert(JSON.stringify(json_data.value.joke));
+});
+}
